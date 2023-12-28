cce5a120acfa723e1b997c088d3cacbfa7961967
Luqman Rasat <luqmanrasat@goodpeople.my>
2023-12-29 03:15:00 +0800

    update telescope extensions config

--- lua/custom/plugins/telescope-extensions.lua
+++ lua/custom/plugins/telescope-extensions.lua
@@ -1,28 +1,28 @@
 return {
 	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({})
-					}
+		'nvim-telescope/telescope-ui-select.nvim',
+		opts = {
+			extensions = {
+				['ui-select'] = {
+					require('telescope.themes').get_dropdown({})
 				}
-			})
-			require("telescope").load_extension("ui-select")
+			}
+		},
+		config = function()
+			require('telescope').load_extension('ui-select')
 		end
 	},
 	{
-		"debugloop/telescope-undo.nvim",
+		'debugloop/telescope-undo.nvim',
 		keys = {
 			{
-				"<leader>u",
-				"<cmd>Telescope undo<cr>",
-				desc = "Undo history",
+				'<leader>u',
+				'<cmd>Telescope undo<cr>',
+				desc = 'Undo history',
 			},
 		},
 		config = function()
-			require("telescope").load_extension("undo")
+			require('telescope').load_extension('undo')
 		end
 	},
 }
