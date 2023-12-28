611acde1696ea6ae369fe035d11127eac100cfc8
Luqman Rasat <luqmanrasat@goodpeople.my>
2023-12-29 03:16:21 +0800

    update neo-tree config

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,18 +1,20 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
+	'nvim-neo-tree/neo-tree.nvim',
+	branch = 'v3.x',
 	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons',
+		'MunifTanjim/nui.nvim',
 	},
 	opts = {
-		filesystem = { hijack_netrw_behavior = "open_current" }
+		filesystem = { hijack_netrw_behavior = 'open_current' }
 	},
-	config = function(_, opts)
-		require("neo-tree").setup(opts)
+	config = function()
+		require('which-key').register {
+			['<leader>e'] = { name = 'File [E]xplorer', _ = 'which_key_ignore' },
+		}
 
-		vim.keymap.set("n", "<leader>ee", ":Neotree focus<CR>", { desc = "file [E]xplorer show/focus" })
-		vim.keymap.set("n", "<leader>eq", ":Neotree close<CR>", { desc = "file [E]xplorer [C]lose" })
+		vim.keymap.set('n', '<leader>ee', ':Neotree focus<CR>', { desc = 'File [E]xplorer show/focus' })
+		vim.keymap.set('n', '<leader>eq', ':Neotree close<CR>', { desc = 'File [E]xplorer [C]lose' })
 	end
 }
