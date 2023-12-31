0f0314067bee2eaf774eaa7a52a3ae15233e4643
Luqman Rasat <luqmanrasat@goodpeople.my>
2023-12-31 14:53:32 +0800

    install golang formatters

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -9,6 +9,8 @@ return {
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.golines,
 			},
 		}
 
